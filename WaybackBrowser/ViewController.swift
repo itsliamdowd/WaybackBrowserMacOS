00640103830cfd50997b58a3472ac5c5ebdf1ca5
SkiingIsFun123 <101684827+SkiingIsFun123@users.noreply.github.com>
2022-06-14 10:41:23 -0700

    Initial commit that works properly

--- WaybackBrowser/ViewController.swift
+++ WaybackBrowser/ViewController.swift
@@ -10,7 +10,6 @@ import WebKit
 import PythonKit
 import SwiftExec
 
-
 class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTextFieldDelegate {
 
     @IBOutlet var webView: WKWebView!
@@ -20,14 +19,12 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         self.webView.uiDelegate = self
         self.webView.navigationDelegate = self
-        let url = URL(string: "http://127.0.0.1:5000/home")
+        let url = Bundle.main.url(forResource: "home", withExtension: "html")
         let request = URLRequest(url: url!)
         webView.load(request)
         self.searchBar.stringValue = "https://google.com"
-        //self.searchBar.delegate = self
     }
 
     @IBAction func back(_ sender: Any) {
@@ -41,10 +38,29 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, NSTe
     @IBAction func textFieldAction(sender: NSTextField) {
         print(sender.stringValue)
         print("RETURNED")
-        var urlstr = "http://127.0.0.1:5000/loadpage?url=" + self.searchBar.stringValue + "&year=2001&month=10&day=10"
-        let url = URL(string: urlstr)
-        let request = URLRequest(url: url!)
-        webView.load(request)
-        //self.searchBar.stringValue = "https://google.com"
+        do{
+            guard let filePath = Bundle.main.path(forResource: "loadpage", ofType: "html") else { return }
+            let contents = try String(contentsOfFile: filePath, encoding: .utf8)
+            let baseUrl = URL(fileURLWithPath: filePath)
+            webView.loadHTMLString(contents as String, baseURL: baseUrl)
+        } catch {
+            print("HTML File error")
+        }
+        //var urlstr = "http://127.0.0.1:5000/loadpage?url=" + self.searchBar.stringValue + "&year=2001&month=10&day=10"
+        //let url = URL(string: urlstr)
+        //let request = URLRequest(url: url!)
+        //webView.load(request)
     }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("HERE123")
+        //var urlstr = "http://127.0.0.1:5000/loadpage?url=" + self.searchBar.stringValue + "&year=2001&month=10&day=10"
+        print(self.searchBar.stringValue)
+        var urlstr = "https://web.archive.org/web/" + "2001" + "10" + "10" + "043326if_/" + self.searchBar.stringValue
+       print(urlstr)
+       var jsscript = "populate(\"" + urlstr + "\")"
+        //"populate(" + urlstr + ")"
+       webView.evaluateJavaScript(jsscript, completionHandler: nil)
+    }
+    
 }
---
+++ WaybackBrowser/run.swift
@@ -0,0 +1,7 @@
+//
+//  run.swift
+//  WaybackBrowser
+//
+//  Created by Liam Dowd on 6/14/22.
+//
+
