c7edbb9221240d52da0e2aa4218ef35aee830e69
SkiingIsFun123 <101684827+SkiingIsFun123@users.noreply.github.com>
2022-06-14 02:03:37 -0700

    Initial Commit

---
+++ WaybackBrowser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WaybackBrowser
+//
+//  Created by Liam Dowd on 6/14/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
